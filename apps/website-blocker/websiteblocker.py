65f1f016851ac26ad770849c6161dd2e9c6d2d12
Ningareddy Modase <ningareddy.modase@gmail.com>
2020-04-30 09:20:06 +0530

    Website blocker example

---
+++ apps/website-blocker/websiteblocker.py
@@ -0,0 +1,27 @@
+import time
+from datetime import datetime as dt
+hosts_path="hosts"
+#hosts_path=r"C:\Windows\System32\drivers\etc\hosts"
+redirect = "127.0.0.1"
+website_list=['www.facebook.com','facebook','dub119.mail.live.com','www.dub119.mail.live.com']
+
+while True:
+    if dt(dt.now().year,dt.now().month,dt.now().day,9) < dt.now() < dt(dt.now().year,dt.now().month,dt.now().day,16):
+        print('Working hours....')
+        with open(hosts_path,"r+") as file:
+            contents = file.read()
+            for website in website_list:
+                if website in contents:
+                    pass
+                else:
+                    file.write(redirect+"    "+website+"\n")
+    else:
+        with open(hosts_path,"r+") as file:
+            contents = file.readlines()
+            file.seek(0)
+            for line in contents:
+                if not any(website in line for website in website_list):
+                    file.write(line)
+            file.truncate()
+        print('Non-Working hours....')
+    time.sleep(5)
